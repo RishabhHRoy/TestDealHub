3938a65beb43b2875f77c1f00e42d38c30a4538e
Rishabh Harshadray roy <riro002@ess.ais.ac.nz>
2023-03-18 00:47:08 +1300

    Package_update

--- ShoppingCart/Models/ErrorViewModel.cs
+++ ShoppingCart/Models/ErrorViewModel.cs
@@ -2,9 +2,8 @@ namespace ShoppingCart.Models
 {
         public class ErrorViewModel
         {
-#nullable enable
+
         public string? RequestId { get; set; }
-#nullable disable
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         }
